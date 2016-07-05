6bc6f2f1921e8966ca3cd07fd125481bf349f959
Akul Umamageswaran <akulum2002@gmail.com>
2016-07-05 16:51:39 -0400

    largest-prime-factor.cpp
    Solution code to Project Euler Problem 3. Finds the largest prime factor of a number.

---
+++ math-programs/largest-prime-factor.cpp
@@ -0,0 +1,38 @@
+/*
+Program created by Akul Umamageswaran.
+Solution code to Project Euler Problem 3. Finds the largest prime factor of a number.
+*/
+
+#include <iostream>
+#include <cstdint>
+using namespace std;
+
+int main() {
+    int64_t num;
+    int64_t largest = 0;
+    
+    cin >> num;
+    int64_t halfNum = num / 2;
+    
+    for (long int i = 2; i <= halfNum; i++) {
+        bool isPrime = true;
+        if (num % i == 0) {
+            for (long int a = 2; a <= i-1; a++) {
+                if (i % a != 0) {
+                    continue;
+                }
+                else {
+                    isPrime = false;
+                    break;    
+                }    
+            }
+            if (isPrime && i > largest) {
+                largest = i;
+            }
+        }
+    }
+    
+    cout << largest;
+    
+    return 0;
+}
