ddecf167dde62b0d7e487cfa56c9d0650b168090
Akul Umamageswaran <akulum2002@gmail.com>
2016-07-06 12:22:24 -0400

    gradebook.cpp

--- useful-programs/gradebook.cpp
+++ useful-programs/gradebook.cpp
@@ -1,21 +1,24 @@
 /*
 Program created by Akul Umamageswaran.
-This program is a "gradebook" that calculates a student's average grade based on seven inputted grades.
+This program is a "gradebook" that calculates a student's average grade based on N inputted grades.
 */
 
 #include <iostream>
 using namespace std;
 
 int main() {
-  int gradebook[7] = {100};
+  int N;
+  cout << "How many grades? ";
+  cin >> N;
+  int gradebook[N] = {100};
   int sum = 0;
-  for (int i = 1; i < 8; i++) {
+  for (int i = 1; i < N+1; i++) {
     cout << "Enter grade for assignment number " << i << ":" << "\n";
     cin >> gradebook[i-1];
     sum += gradebook[i-1];
   }
   
-  int average = sum / 7;
+  int average = sum / N;
   cout << "The student's average is: " << average << ".";
   
   return 0;
